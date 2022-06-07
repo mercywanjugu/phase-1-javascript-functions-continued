08b7390b055ce36a9b198bb7e085163dcd49e1cf
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-07 14:43:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+
+
+function saturdayFun(toDo = "roller-skate") {
+    return `This Saturday, I want to ${toDo}!`;
+  }
+  
+  saturdayFun("bathe my dog!");
+  
+  // mondayWork/
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // wrapAdjective/
+  
+  function wrapAdjective(style = "||") {
+    return function (innerParam = "special") {
+      return `You are ${style}${innerParam}${style}!`;
+    };
+  }
